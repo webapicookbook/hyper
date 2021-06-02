f2fc7970a65b9ea9a1373db031233b6bc06a55d3
mamund <mamund@yahoo.com>
2021-06-01 21:00:39 -0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,24 +4,46 @@
  *
  * ********************************************/
 
-
+const axios = require('axios');
+const request = require('request');
 const readline = require('readline');
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: 'Enter blank for timestamp, timestamp for date or exit> '
+  prompt: '[?]> '
 });
+
+var responses = [];
 
 rl.prompt();
 
 rl.on('line', (line) => {
-  if (line.toLowerCase() === "exit") {
-    console.log('Exiting!\n');
-    process.exit(0);        
-  } else if (line.trim() === "") {
-    console.log(timeStamp()+"\n");
-  } else {
-    console.log(dateTime(line)+"\n");
+  line = line.trim();
+  var words = line.split(" ");
+  switch (words[0].toUpperCase()) {
+    case "EXIT":
+    case "STOP":
+      console.log("EXITING!\n");
+      process.exit(0)
+      break;
+    case "ACTIVATE":
+      activate(words);  
+      console.log("working...");
+      break;
+    case "DISPLAY":
+      console.log(display(words));
+      break;
+    case "RESPONSES":
+      console.log(responses.length);
+      break;
+    case "TIMESTAMP":
+      console.log(timeStamp(line)+"\n");
+      break;
+    case "ECHO":  
+    default:
+      console.log(echo(words)+"\n");
+      break;
   }
   rl.prompt();
 
@@ -30,11 +52,72 @@ rl.on('line', (line) => {
   process.exit(0);
 });
 
-function timeStamp() {
-  return Date.now().toString(36)
+// echo the command line
+function echo(words) {
+  rt = "";
+  words.forEach(function peek(w) {
+    rt += "word="+w+"\n";
+  });
+  return rt;
 }
 
-function dateTime(line) {
-  return new Date(parseInt(line, 36)).toString();
+// display a saved response
+// DISPLAY LENGTH returns number of responses saved
+// DISPLAY {index} returns the response at that index
+// DISPLAY returns top response in the collection
+function display(words) {
+  var rt = "";
+  var index = 0;
+  var token = words[1]||"0";
+  
+  switch (token.toLowerCase()) {
+    case "len":
+    case "length":
+      rt = responses.length;
+      break;
+    default:
+      index = parseInt(token);  
+      if(index<0) {index=0};
+      if(index>responses.length-1) {index=responses.length-1};
+      rt = responses[index];
+  }
+  return rt;
+}
+
+// perform the HTTP query
+function activate(words) {
+  var rt = "working...";
+  var url = words[1]||"#";
+  var headers = {};
+  
+  if(words[2] && words[2].toUpperCase()==="WITH") {
+    switch (words[3].toLowerCase()) {
+      case "headers":
+        try {
+          headers = JSON.parse(words[4])
+        } catch {
+          headers = {};
+        }  
+        break;
+    }
+  }
+  
+  (async () => {
+    try {
+      const response = await axios(url,{headers:headers});
+      responses.push(response);
+      console.log(response);
+      console.log("\n");
+    }
+    catch (err) {
+      console.log(err);
+    }
+  })();
+  return rt;
+}
+
+// generate a unique string based on date/time
+function timeStamp() {
+  return Date.now().toString(36)
 }
 
