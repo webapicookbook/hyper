dba19b0260c3c5a41369773245c982f811c63bd2
mamund <mamund@yahoo.com>
2021-06-19 14:58:50 -0400

    remove old hal code

--- src/index.js
+++ src/index.js
@@ -249,67 +249,6 @@ function sirenCommands(words) {
   return JSON.stringify(rt, null, 2);
 }
 
-// display and parse a HAL response
-// HAL {command}
-/*
-function halCommands(words) {
-  var rt = {};
-  var token = words[1]||"";
-  var response;
-  
-  try {
-    response = responses.peek();
-  } catch {
-    token="";
-  }
-  
-  switch (token.toUpperCase()) {
-    case "LINKS":
-    case "_LINKS":
-      rt = JSON.parse(response.getBody('UTF8'))._links;
-      break;
-    case "EMBEDDED":
-    case "_EMBEDDED":
-      rt = JSON.parse(response.getBody('UTF8'))._embedded;
-      break;
-    case "REL":
-    case "ID":
-    case "KEY":  
-      token  = "$..*[?(@property==='"+words[2]+"')]";
-      if("rel id key".toLowerCase().indexOf(token.toLowerCase())==-1) {
-        try {
-          rt = JSON.parse(response.getBody('UTF8'));
-          rt = JSONPath({path:token, json:rt});
-        } catch {
-          // no-op
-        }
-      }  
-      else {
-        rt = "no response";
-      }  
-      break;
-    case "PATH":  
-      token = words[2]||"$";
-      console.log(token);
-      try {
-        rt = JSON.parse(response.getBody('UTF8'));
-        rt = JSONPath({path:token, json:rt});
-      } catch {
-        // no-op
-      }
-      break;
-    default:  
-      try {
-        response = responses.peek()
-        rt = JSON.parse(response.getBody("UTF8"));
-      } catch {
-        rt = "no response";
-      }
-  }
-  return JSON.stringify(rt, null, 2);
-}
-*/
-
 // synchronous HTTP request
 // ACTIVATE {url}
 // - WITH-HEADERS {n:v,...}
