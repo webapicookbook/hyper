9b12494118fb132f0e706f8f3daec236e239832f
mamund <mamund@yahoo.com>
2021-06-18 23:23:59 -0400

    finish config move

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const rl = readline.createInterface({
 
 var config = {};
 config.verbose = "false";
-var args = configOp({config:config,words:["LOAD"]});
+var args = configOp({config:config,words:["CONFIG", "LOAD"]});
 config = args.config;
 
 var currentResponse = {};
@@ -324,97 +324,6 @@ function dsSet(token) {
   }  
   return item;
 }
-
-// configuration operations
-/*
-function configOp(words) {
-  var rt = "";
-  var file = "";
-  var set = {};
-  var data = "";
-  var token = words[1]||"";
-  
-  switch (token.toUpperCase()) {
-    case "FILE":
-    case "LOAD":
-      rt = configLoad(words[2]||"");
-      break;
-    case "WRITE":
-    case "SAVE":
-      rt = configSave(words[2]||"");  
-      break;
-    case "SET":
-      rt = configSet(words[2]);
-      break;  
-    case "READ":
-    default:
-      rt = config  
-  }
-  return rt;
-}
-*/
-
-// save config file to disk
-function configSave(file) {
-  var rt = "";
-  var data = "";
-  var target = "";
-  data = JSON.stringify(config, null, 2);
-  target = file||"./hyper.cfg";
-  try {
-    fs.writeFileSync(target,data);
-    rt = "config saved as ["+target+"]";
-  } catch (err) {
-    rt = "ERR: " + console.error(err);
-  }
-  return rt;
-}
-
-// load config from file
-// overwrites any existing settings of the same name
-function configLoad(file) {
-  var rt = "";
-  var set = {};
-  var data = "";
-  var target = file||"./hyper.cfg";
-  
-  try {
-    console.log(target);
-    if(target==="") {target = "./hyper.cfg"};
-    if(fs.existsSync(target)) {
-      data = fs.readFileSync(target, {encoding:'utf8', flag:'r'});
-      set = JSON.parse(data);
-      for(var c in set) {
-        config[c] = set[c];
-      }
-      rt = config;
-    }
-    else {
-      rt = "can't open ["+target+"]";
-    }  
-  } catch(err) {
-    rt = "ERR: "+console.error(err);
-  }
-  return rt;
-}
-
-// set config values
-// CONFIG-SET {n:v,...}
-function configSet(token) {
-  var rt = "";
-  var set = {};
-  
-  try {
-    set = JSON.parse(token);
-    for(var c in set) {
-    config[c] = set[c];
-    }
-  } catch {
-    // no-op
-  }  
-  return config;
-}
-
 
 // display and parse a SIREN response
 // SIREN {command}
